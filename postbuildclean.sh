d49883cc4d4986e11ca66058d5a327691e6e048a
Ruchika K <ruchika.kharwar@gmail.com>
2019-01-15 14:22:15 +0000

    Bugfix: Ensure proper clean up
    Signed-off-by: Ruchika K <ruchika.kharwar@gmail.com>

--- postbuildclean.sh
+++ postbuildclean.sh
@@ -3,3 +3,4 @@ rm -fr out-genfiles
 rm -fr out-bin
 rm ./py/google/fhir/seqex/bundle_to_seqex_converter.so
 rm ./py/google/fhir/seqex/bundle_to_seqex_util.so
+rm ./py/google/fhir/seqex/bundle_to_seqex_converter.py
